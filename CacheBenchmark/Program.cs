47a0a361a75a48d257082a8230ee2d2f1a3992f8
Florian Feka <47530143+FlorianFeka@users.noreply.github.com>
2021-06-12 13:13:41 +0200

    add project

---
+++ CacheBenchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CacheBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
