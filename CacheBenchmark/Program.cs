222e30875bc566c87d8455ba835c04d2f54ba39c
Alexenia Weibel <if19b178@technikum-wien.at>
2021-06-15 17:28:44 +0200

    Add Merge Sort

--- CacheBenchmark/Program.cs
+++ CacheBenchmark/Program.cs
@@ -12,25 +12,43 @@ namespace CacheBenchmark
             var list = bubbleSort.DoBubbleSort(bubbleArray);
 
             Console.WriteLine("Bubble Sort: ");
-            foreach (var item in list)
-            {
-                Console.Write(item + " ");
-            }
+            PrintArray(bubbleArray);
 
+            // Merge Sort
+            int[] mergeArray = { 12, 11, 13, 5, 6, 7 };
+
+            MergeSort ob = new MergeSort();
+            ob.Sort(mergeArray, 0, mergeArray.Length - 1);
+            Console.WriteLine("\nMerge Sort:");
+            PrintArray(mergeArray);
+  
             // Bucket Sort
             var bucketSort = new BucketSort();
-            float[] arr = { (float)0.897, (float)1, (float)0.565,
+            float[] bucketArray = { (float)0.897, (float)1, (float)0.565,
                    (float)0.656, (float)0.1234,
                    (float)0.665, (float)0.3434 };
 
-            int n = arr.Length;
-            bucketSort.Sort(arr, n);
+            int n = bucketArray.Length;
+            bucketSort.Sort(bucketArray, n);
 
-            Console.WriteLine("\n\nBucket Sort: ");
-            foreach (var item in arr)
-            {
-                Console.Write(item + " ");
-            }
+            Console.WriteLine("\nBucket Sort: ");
+            PrintArray(bucketArray);
+        }
+
+        public static void PrintArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+
+        public static void PrintArray(float[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
         }
     }
 }
