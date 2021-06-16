c9e3f6d1b97038862402719ecf7a880ad4162292
Florian Feka <47530143+FlorianFeka@users.noreply.github.com>
2021-06-16 13:11:38 +0200

    add bubble sort benchmark

--- CacheBenchmark/Benchmarks.cs
+++ CacheBenchmark/Benchmarks.cs
@@ -1,12 +1,12 @@
 ﻿using BenchmarkDotNet.Attributes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Jobs;
 
 namespace CacheBenchmark
 {
+    [SimpleJob(runtimeMoniker: RuntimeMoniker.Net50, baseline: true, launchCount: 1, warmupCount: 1, targetCount: 1, invocationCount: 1, runStrategy: RunStrategy.ColdStart)]
+    //[SimpleJob(runtimeMoniker: RuntimeMoniker.NetCoreApp31, launchCount: 1, warmupCount: 1, targetCount: 1, invocationCount: 1, runStrategy: RunStrategy.ColdStart)]
+    //[SimpleJob(runtimeMoniker: RuntimeMoniker.Net48, launchCount: 1, warmupCount: 1, targetCount: 1, invocationCount: 1, runStrategy: RunStrategy.ColdStart)]
     public class Benchmarks
     {
         private int[] intArray{get;set;}
@@ -18,8 +18,8 @@ namespace CacheBenchmark
             this.intArray = new int[] { 2, 7, 1 };
         }
 
-        [Benchmark]
-        public void DoBenchmarkTests()
+        [Benchmark(Baseline = true)]
+        public void BubbleSort()
         {          
             bubbleSort.Sort(this.intArray);
         }
